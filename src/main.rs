47881ec3c940239964745a093e8ca3e51b974fc0
M*C*O <mcofficer@gmx.de>
2020-04-19 15:17:32 +0200

    Immature logging implementation, except this time it works

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 #![forbid(unsafe_code)]
 
-//mod archive;
-//mod github;
-//mod init_view;
+mod archive;
+mod github;
 mod install;
-//mod music;
+mod music;
+mod worker;
 
-use iced::{button, executor, Align, Application, Button, Column, Command, Element, Image, Sandbox, Settings, Subscription, Text, ProgressBar, Container, Length};
+use crate::worker::{Work, Worker};
+use iced::{button, Align, Button, Column, Container, Element, Length, Sandbox, Settings, Text};
 use nfd2::Response;
 use std::path::PathBuf;
 
@@ -18,203 +19,79 @@ pub fn main() {
 }
 
 #[derive(Debug)]
-enum ESLauncher {
-    PreInstall {
-        destination: PathBuf,
-        destination_chooser: button::State,
-        install_button: button::State,
-    },
-    Installing {
-        progress: f32,
-    },
+struct ESLauncher {
+    destination: PathBuf,
+    destination_chooser: button::State,
+    install_button: button::State,
+    worker: Option<worker::Worker>,
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 enum Message {
     SelectDestination,
     StartInstallation,
 }
 
-impl Application for ESLauncher {
-    type Executor = executor::Default;
+impl Sandbox for ESLauncher {
     type Message = Message;
-    type Flags = ();
 
-    fn new(_flags: ()) -> (ESLauncher, Command<Message>) {
-        (
-            ESLauncher::PreInstall {
-                destination: PathBuf::default(),
-                destination_chooser: button::State::default(),
-                install_button: button::State::default(),
-            },
-            Command::none(),
-        )
+    fn new() -> ESLauncher {
+        ESLauncher {
+            destination: PathBuf::default(),
+            destination_chooser: button::State::default(),
+            install_button: button::State::default(),
+            worker: None,
+        }
     }
 
     fn title(&self) -> String {
         String::from("ESLauncher2")
     }
 
-    fn update(&mut self, message: Self::Message) -> Command<Message> {
+    fn update(&mut self, message: Self::Message) {
         match message {
             Message::SelectDestination => {
-                match self {
-                    ESLauncher::PreInstall { destination, .. } => {
-                        match nfd2::open_pick_folder(None).unwrap() {
-                            Response::Okay(path) =>  *destination = path,
-                            _ => (),
-                        }
-                    }
-                    _ => {}
+                if let Response::Okay(path) = nfd2::open_pick_folder(None).unwrap() {
+                    self.destination = path;
                 }
-            },
+            }
             Message::StartInstallation => {
-                match self {
-                    ESLauncher::PreInstall { destination, .. } => {
-                        *self = ESLauncher::Installing {
-                            progress: 0.0
-                        }
-                    }
-                    _ => {}
+                if self.worker.is_none() {
+                    self.worker = Some(Worker::new(Work::Install {
+                        destination: self.destination.clone(),
+                    }));
                 }
-            },
-        }
-        Command::none()
-    }
-
-    fn subscription(&self) -> Subscription<Message> {
-        match self {
-            ESLauncher::Installing { .. } => Subscription::none(),
-            _ => Subscription::none(),
-        }
+            }
+        };
     }
 
     fn view(&mut self) -> Element<'_, Self::Message> {
-        let content = match self {
-            ESLauncher::PreInstall {destination, destination_chooser, install_button } => {
-                 Column::new()
-                    .padding(20)
-                    .align_items(Align::Center)
-                    .push(Text::new(destination.to_string_lossy()))
-                    .push(
-                        Button::new(destination_chooser, Text::new("Pick Folder"))
-                            .on_press(Message::SelectDestination),
-                    )
-                    .push(
-                        Button::new(install_button, Text::new("Install"))
-                            .on_press(Message::StartInstallation),
-                    )
-            }
-
-            ESLauncher::Installing { progress } => {
-                Column::new()
-                    .padding(20)
-                    .align_items(Align::Center)
-                    .push(ProgressBar::new(0.0..=100.0, *progress))
-            }
+        let logbox = match &self.worker {
+            Some(worker) => self.worker.as_mut().unwrap().logs().iter().cloned().fold(
+                Column::new().padding(20).align_items(Align::Center),
+                |column, log| column.push(Text::new(log)),
+            ),
+            None => Column::new(),
         };
+        let content = Column::new()
+            .padding(20)
+            .align_items(Align::Center)
+            .push(Text::new(self.destination.to_string_lossy()))
+            .push(
+                Button::new(&mut self.destination_chooser, Text::new("Pick Folder"))
+                    .on_press(Message::SelectDestination),
+            )
+            .push(
+                Button::new(&mut self.install_button, Text::new("Install"))
+                    .on_press(Message::StartInstallation),
+            )
+            .push(logbox);
 
         Container::new(content)
             .width(Length::from(WIDTH))
             .height(Length::from(HEIGHT))
-            .center_x().center_y().into()
+            .center_x()
+            .center_y()
+            .into()
     }
 }
-
-/*
-impl MainViewState {
-    fn action(&self, action: impl Into<Option<Action>>) {
-        self.action.set(action.into());
-    }
-}
-
-impl State for MainViewState {
-    fn update(&mut self, _: &mut Registry, ctx: &mut Context<'_>) {
-        if let Some(action) = self.action.take() {
-            match action {
-                Action::SelectDestination(path) => {
-                    ctx.child("install-button").set("enabled", true);
-                    ctx.child("destination-text")
-                        .set("text", String16::from(String::from(path.to_string_lossy())));
-                    self.destination.replace(path);
-                }
-                Action::StartInstallation() => {
-                    ctx.child("install-button").set("enabled", false);
-                    println!("test");
-                    install::install(&self.destination.borrow());
-                }
-                Action::Log(msg) => {
-                    let child = TextBlock::create().class("log-line").text(msg);
-                    let entity = ctx.entity_of_child("log-box").unwrap();
-                    ctx.append_child_to(child, entity);
-                    ctx.child("log-scroll")
-                        .set("scroll_offset", Point::new(0.0, std::f64::MIN));
-                }
-            }
-        }
-    }
-}
-
-widget!(MainView<MainViewState>);
-
-impl Template for MainView {
-    fn template(self, id: Entity, ctx: &mut BuildContext) -> Self {
-        self.name("MainView").child(
-            Grid::create()
-                .rows(Rows::create().row(215.0).row("*").build())
-                .id("content")
-                .child(
-                    ImageWidget::create()
-                        .attach(Grid::row(0))
-                        .image(Image::from_data(460, 215, header_image()).unwrap())
-                        .build(ctx),
-                )
-                .child(
-                    Container::create()
-                        .id("content")
-                        .attach(Grid::row(1))
-                        .child(init_view::create(id, ctx))
-                        .build(ctx),
-                )
-                .build(ctx),
-        )
-    }
-}
-
-fn main() {
-    music::play();
-
-    Application::new()
-        .window(|ctx| {
-            Window::create()
-                .title("ESLauncher2")
-                .position((500.0, 100.0))
-                .size(WIDTH, HEIGHT)
-                .resizeable(true)
-                .theme(
-                    ThemeValue::create_from_css(DEFAULT_THEME_CSS)
-                        .extension_css(include_str!("../assets/style.css"))
-                        .build(),
-                )
-                .child(MainView::create().build(ctx))
-                .build(ctx)
-        })
-        .run();
-}
-
-fn header_image() -> Vec<u32> {
-    let array: &[u8] = include_bytes!("../assets/header.jpg");
-    let img = image::load_from_memory_with_format(array, image::ImageFormat::Jpeg).unwrap();
-    img.into_rgba()
-        .pixels()
-        .map(|p| {
-            ((p[3] as u32) << 24) | ((p[0] as u32) << 16) | ((p[1] as u32) << 8) | (p[2] as u32)
-        })
-        .collect()
-}
-
-// helper to request MainViewState
-fn state<'a>(id: Entity, states: &'a mut StatesContext) -> &'a mut MainViewState {
-    states.get_mut(id)
-}
- */
---
+++ src/worker.rs
@@ -0,0 +1,48 @@
+use crate::install;
+use std::borrow::Borrow;
+use std::path::PathBuf;
+use std::sync::mpsc;
+use std::sync::mpsc::Receiver;
+use std::thread;
+use std::thread::JoinHandle;
+
+#[derive(Debug, Clone)]
+pub enum Work {
+    Install { destination: PathBuf },
+}
+
+#[derive(Debug)]
+pub struct Worker {
+    receiver: Receiver<String>,
+    handle: JoinHandle<()>,
+    logs: Vec<String>,
+    work: Work,
+}
+
+impl Worker {
+    pub fn new(work: Work) -> Worker {
+        let (sender, receiver) = mpsc::channel();
+
+        let func = match &work {
+            Work::Install { destination } => {
+                let destination = destination.clone();
+                move || install::install(sender, destination)
+            }
+        };
+        let handle = thread::spawn(func);
+        Worker {
+            receiver,
+            handle,
+            logs: vec![],
+            work,
+        }
+    }
+
+    pub fn logs(&mut self) -> &Vec<String> {
+        let optional = self.receiver.try_recv().ok(); // TODO: React to disconnects
+        if let Some(log) = optional {
+            self.logs.push(log)
+        };
+        &self.logs
+    }
+}
