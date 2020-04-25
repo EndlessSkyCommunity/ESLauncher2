d5e0b4974d2f56bd411d109045f6342baf747977
M*C*O <mcofficer@gmx.de>
2020-04-25 15:17:18 +0200

    Hopefully don't crash if the music system fails to initialize

--- src/music.rs
+++ src/music.rs
@@ -1,9 +1,12 @@
 use std::io::{BufReader, Cursor};
+use std::panic;
 
 pub fn play() -> Option<()> {
-    let device = rodio::default_output_device()?;
-    let song: &[u8] = include_bytes!("../assets/endless-prototype.ogg");
-    let reader = BufReader::new(Cursor::new(song));
-    rodio::play_once(&device, reader).ok()?.detach();
-    Some(())
+    panic::catch_unwind(|| {
+        let device = rodio::default_output_device().unwrap();
+        let song: &[u8] = include_bytes!("../assets/endless-prototype.ogg");
+        let reader = BufReader::new(Cursor::new(song));
+        rodio::play_once(&device, reader).unwrap().detach();
+    })
+    .ok()
 }
