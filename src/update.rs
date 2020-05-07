5198cd2f20069afbe52eed34f507656e8a9f2274
M*C*O <mcofficer@gmx.de>
2020-05-07 09:14:10 +0200

    Fixed issues related to bitar

--- src/update.rs
+++ src/update.rs
@@ -5,7 +5,6 @@ use anyhow::Result;
 use bitar::{clone_from_archive, clone_in_place, Archive, CloneOptions, ReaderRemote};
 use std::path::PathBuf;
 use tokio::fs::OpenOptions;
-use url::Url;
 
 pub async fn update_instance(instance: Instance) -> Result<Instance> {
     if let InstanceType::Unknown = instance.instance_type {
@@ -33,7 +32,16 @@ pub async fn update_instance(instance: Instance) -> Result<Instance> {
                 "https://ci.mcofficer.me/job/EndlessSky-continuous-bitar/lastBuild/artifact/{}.cba",
                 instance.instance_type.archive().unwrap()
             );
-            bitar_update_archive(&archive_path, url).await?;
+            // We need a tokio Runtime because, apparently, OpenOptions and friends are doing some tokio-specific stuff
+            // under the hood. This isn't actually blocking in that it doesn't block the application thread.
+            match tokio::runtime::Runtime::new() {
+                Ok(mut runtime) => {
+                    if let Err(e) = runtime.block_on(bitar_update_archive(&archive_path, url)) {
+                        error!("Failed to update instance: {:#}", e)
+                    }
+                }
+                Err(e) => error!("Failed to spawn tokio runtime: {}", e),
+            };
             if !archive_path.ends_with(InstanceType::AppImage.archive().unwrap()) {
                 archive::unpack(&archive_path, &instance.path)?;
             }
@@ -54,7 +62,8 @@ async fn bitar_update_archive(target_path: &PathBuf, url: String) -> Result<()> 
         .open(&target_path)
         .await?;
 
-    let mut reader = ReaderRemote::new(Url::parse(&url)?, 3, None, None);
+    let client = reqwest::Client::new().get(&url);
+    let mut reader = ReaderRemote::new(client, 3, None);
     let archive = Archive::try_init(&mut reader).await?;
     let mut chunks_left = archive.source_index().clone();
 
