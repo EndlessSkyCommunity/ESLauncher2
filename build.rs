af31812b42ccf087533b884b19cffba769e8b4b5
M*C*O <mcofficer@gmx.de>
2020-06-04 15:55:45 +0200

    Add icon to windows builds

---
+++ build.rs
@@ -0,0 +1,14 @@
+#[cfg(windows)]
+extern crate winres;
+
+#[cfg(windows)]
+fn main() {
+    let mut res = winres::WindowsResource::new();
+    res.set_icon("icon.ico");
+    res.set_language(0x0009);
+    res.set("ProductName", "ESLauncher2");
+    res.compile().unwrap();
+}
+
+#[cfg(unix)]
+fn main() {}
