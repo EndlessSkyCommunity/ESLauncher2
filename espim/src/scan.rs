3cf6d305a67e1983a529d1fb53b7d53853f1d3b2
M*C*O <mcofficer@gmx.de>
2020-06-05 14:43:33 +0200

    ESPIM: Don't fail initialization if the local folder doesn't exist yet

--- espim/src/scan.rs
+++ espim/src/scan.rs
@@ -4,20 +4,23 @@ use std::fs;
 use std::path::PathBuf;
 
 /// Attempts to read plug-ins from the default directory
-// One-line city, hell yeah.
 pub(crate) fn scan_plugins() -> Result<Vec<InstalledPlugin>> {
-    Ok(es_plugin_dir()
-        .ok_or_else(|| anyhow!("Failed to get ES Plug-In dir"))?
-        .read_dir()?
-        .filter_map(|res| {
-            let res = res.ok()?;
-            let version = read_version(&res.path()).unwrap_or_else(|_| String::from("unknown"));
-            Some(InstalledPlugin {
-                name: String::from(res.file_name().to_str()?),
-                version,
+    let plugin_dir = es_plugin_dir().ok_or_else(|| anyhow!("Failed to get ES Plug-In dir"))?;
+    Ok(if plugin_dir.exists() {
+        plugin_dir
+            .read_dir()?
+            .filter_map(|res| {
+                let res = res.ok()?;
+                let version = read_version(&res.path()).unwrap_or_else(|_| String::from("unknown"));
+                Some(InstalledPlugin {
+                    name: String::from(res.file_name().to_str()?),
+                    version,
+                })
             })
-        })
-        .collect())
+            .collect()
+    } else {
+        vec![]
+    })
 }
 
 fn read_version(plugin_dir: &PathBuf) -> std::io::Result<String> {
