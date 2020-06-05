26802075dc5bb0d0b5777000efbd415725c2dc78
M*C*O <mcofficer@gmx.de>
2020-06-05 14:36:42 +0200

    Log to terminal, file and GUI

--- src/logger.rs
+++ src/logger.rs
@@ -1,5 +1,8 @@
-use env_logger::Logger;
-use log::{Level, LevelFilter, Log, Metadata, Record};
+use log::{Level, Log, Metadata, Record};
+use simplelog::{
+    CombinedLogger, Config, LevelFilter, SharedLogger, TermLogger, TerminalMode, WriteLogger,
+};
+use std::fs::File;
 use std::sync::mpsc;
 
 const BLACKLIST: [&str; 4] = ["gfx_backend_", "winit", "wgpu_", "iced_"];
@@ -28,6 +31,20 @@ impl Log for ChanneledLogger {
     fn flush(&self) {}
 }
 
+impl SharedLogger for ChanneledLogger {
+    fn level(&self) -> LevelFilter {
+        LevelFilter::Warn
+    }
+
+    fn config(&self) -> Option<&Config> {
+        None
+    }
+
+    fn as_log(self: Box<Self>) -> Box<dyn Log> {
+        unimplemented!()
+    }
+}
+
 fn should_log(record: &Record) -> bool {
     match record.module_path() {
         Some(path) => {
@@ -42,14 +59,24 @@ fn should_log(record: &Record) -> bool {
     }
 }
 
+fn open_logfile() -> File {
+    let mut path = std::env::current_dir().unwrap();
+    path.push("ESLauncher2.log");
+    File::create(path).unwrap()
+}
+
 pub fn init() -> mpsc::Receiver<String> {
     let (log_writer, log_reader) = mpsc::sync_channel(128);
 
-    let channeled = Box::new(ChanneledLogger {
+    let channeled = ChanneledLogger {
         channel: log_writer,
-    });
-    let env = Box::new(env_logger::builder().build());
-    multi_log::MultiLogger::init(vec![channeled, env], log::Level::Debug).unwrap();
+    };
+    CombinedLogger::init(vec![
+        Box::new(channeled),
+        TermLogger::new(LevelFilter::Info, Config::default(), TerminalMode::Mixed),
+        WriteLogger::new(LevelFilter::Info, Config::default(), open_logfile()),
+    ])
+    .unwrap();
 
     log::info!("Initialized logger");
     log_reader
